d6f50a6337259ef951d68d6d5ec3137f92d6aab0
vitoriaemilly2007 <160539668+vitoriaemilly2007@users.noreply.github.com>
2025-09-16 20:44:33 +0000

    aula2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +8,15 @@ def index():
 
 @app.route('/contato')
 def cotato():
-    return 'alba.lops@ifrn.edu.br'
+    return '<h1>alba.lops@ifrn.edu.br<h1/>'
+    
+@app.route('/exemplo')
+def exemplo():
+    return render_template('exemplo.html')
+
+@app.route('/exemplo2')
+def exemplo2():
+    return render_template('exemplo2.html')
 
 if __name__== '__main__':
     app.run()
